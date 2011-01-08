9d95bb22d36d3ea6512a7d7ad77abb207f47cc73
Rudy Rigot <rudy.rigot@gmail.com>
2011-01-08 01:54:29 +0100

    Command line parsing

--- src/com/rudyrigot/staticwebsitecompiler/StaticWebsiteCompiler.java
+++ src/com/rudyrigot/staticwebsitecompiler/StaticWebsiteCompiler.java
@@ -1,9 +1,101 @@
 package com.rudyrigot.staticwebsitecompiler;
 
+import java.lang.reflect.Constructor;
+
+import com.rudyrigot.staticwebsitecompiler.batch.BatchScripter;
+
 public class StaticWebsiteCompiler {
 	
+	private static String filename;
+	private static int version;
+	private static BatchScripter batchScripter;
+	
 	public static void main (String [] args) {
-		System.out.println("That's a start!");
+		System.out.println("**************************************************");
+		System.out.println("* StaticWebsiteCompiler is your new best friend! *");
+		System.out.println("**************************************************");
+		System.out.println();
+
+		// parsing the command line
+		if (args.length%2!=0) usage(null);
+		int i=0;
+		while (i<args.length) {
+			String operation = args[i];
+			String argument = args[i+1];
+			if (!operation.startsWith("-")) usage(operation+" is not a valid operation (should start with '-')");
+			operation = operation.substring(1);
+			if (operation.equals("f")) checkFilename(argument);
+			else if (operation.equals("v")) checkVersion(argument);
+			else if (operation.equals("b")) checkBatchScripter(argument);
+			else usage(operation+" is not a valid operation : only -f, -v, and -b exist");
+			i+=2;
+		}
+		
+		
+		
+	}
+	
+	/**
+	 * Checking and storing the argument passed as a filename 
+	 * 
+	 * @param argument
+	 */
+	private static void checkFilename(String argument) {
+		filename = argument;
+		System.out.println("File: "+filename+(!filename.endsWith(".html")?"  (warning: doesn't end in .html, but i guess you know that)":""));
+	}
+	
+	/**
+	 * Checking and storing the argument passed as a version
+	 * 
+	 * @param argument
+	 */
+	private static void checkVersion(String argument) {
+		try {
+			version = Integer.parseInt(argument);
+			System.out.println("Version: "+version);
+		}
+		catch (Exception e) {
+			usage("Problem with version number parsing (must be a number, eh?)");
+		}
+	}
+	
+	/**
+	 * Checking and storing the argument passed as BatchScripter classname
+	 * 
+	 * @param argument
+	 */
+	private static void checkBatchScripter(String argument) {
+		Class batchScripterClass=null;
+		try {
+			batchScripterClass = Class.forName(argument);
+		}
+		catch (ClassNotFoundException e) {
+			usage("The class "+argument+" does not exist");
+		}
+		BatchScripter batchScripter = null;
+		try {
+			Constructor batchScripterConstructor = batchScripterClass.getConstructor();
+			Object batchScripterObject = batchScripterConstructor.newInstance();
+			batchScripter = (BatchScripter) batchScripterObject;
+		}
+		catch(Exception e) {
+			usage("Problem with the constructor: "+e.getMessage());
+		}
+		StaticWebsiteCompiler.batchScripter = batchScripter;
+		System.out.println("BatchScripter built: "+batchScripter.toString());
+	}
+	
+	private static void usage(String message) {
+		if (message!=null) System.err.println("Error : "+message);
+		System.out.println("Usage : three options");
+		System.out.println(" * -f : if for a file in particular (parses all of them otherwise)");
+		System.out.println(" * -v : if for a version in particular (parses all of them otherwise)");
+		System.out.println(" * -b : for the BatchScripter location (uses the default one otherwise)");
+		System.out.println();
+		System.out.println("Example :");
+		System.out.println("java -jar StaticWebsiteCompiler.jar -f index.html -v 1 -b com.rudyrigot.staticwebsitecompiler.TenorBatchScripter");
+		System.exit(-1);
 	}
 
 }
