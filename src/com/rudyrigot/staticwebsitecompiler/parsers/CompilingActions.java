99306c27b7f24e1a593902bb155a195098260e24
Rudy Rigot <rudy.rigot@gmail.com>
2011-01-08 03:18:05 +0100

    Making sure the CompilingAction knows about the WebsiteVersion

--- src/com/rudyrigot/staticwebsitecompiler/parsers/CompilingActions.java
+++ src/com/rudyrigot/staticwebsitecompiler/parsers/CompilingActions.java
@@ -1,7 +1,9 @@
 package com.rudyrigot.staticwebsitecompiler.parsers;
 
+import com.rudyrigot.staticwebsitecompiler.websiteversion.WebsiteVersion;
+
 public interface CompilingActions {
 	
-	public String executeCompilingActions(String line, String fileName);
+	public String executeCompilingActions(WebsiteVersion version, String line, String fileName);
 
 }
--- src/com/rudyrigot/staticwebsitecompiler/parsers/DefaultCompilingActions.java
+++ src/com/rudyrigot/staticwebsitecompiler/parsers/DefaultCompilingActions.java
@@ -1,8 +1,10 @@
 package com.rudyrigot.staticwebsitecompiler.parsers;
 
+import com.rudyrigot.staticwebsitecompiler.websiteversion.WebsiteVersion;
+
 public class DefaultCompilingActions implements CompilingActions {
 
-	public String executeCompilingActions(String line, String fileName) {
+	public String executeCompilingActions(WebsiteVersion version, String line, String fileName) {
 		/** First, let's execute the most common actions */
 		
 		// Minifying the HTML
@@ -10,12 +12,12 @@ public class DefaultCompilingActions implements CompilingActions {
 		
 		
 		/** Then, let's call for any other extendable stuff */
-		line = extraCompilingActions(line, fileName);
+		line = extraCompilingActions(version, line, fileName);
 		
 		return line;
 	}
 	
-	public String extraCompilingActions(String line, String fileName) {
+	public String extraCompilingActions(WebsiteVersion version, String line, String fileName) {
 		return line;
 	}
 
